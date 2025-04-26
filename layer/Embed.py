77761117475b083305285d1be43c67a4ccf4eef8
DingPF <1017547468@qq.com>
2025-04-26 13:47:07 +0800

    增加位置编码类和相关方法

--- layer/Embed.py
+++ layer/Embed.py
@@ -1,2 +1,25 @@
 import torch
 import torch.nn as nn
+import math
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEmbedding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        pe.requires_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float()
+                    * - (math.log(10000.0) / d_model)).exp()
+
+
+
+        # sin and cos position encoding
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)    # add batch dimension
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
